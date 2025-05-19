8dc3abd9235cb9b803fe9af4d6d2009009d9dd60
Marlon Pons Blanco <mapachestropicales@gmail.com>
2025-05-19 15:12:07 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the', 'our', 'my', 'your', 'his', 'her'];
+let adj = ['great', 'fast', 'crazy', 'funny', 'dark', 'bright', 'silent', 'wild'];
+let noun = ['raccoon', 'tiger', 'developer', 'pirate', 'ninja', 'robot', 'wizard', 'penguin'];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let textresult="";
+  for(let i=0;i<pronoun.length;i++){
+    for(let j=0;j<adj.length;j++){
+      for(let z=0;z<noun.length;z++){
+        textresult+=`${pronoun[i]}`+`${adj[j]}`+`${noun[z]}`+`.com`+"<br>";
+      }
+    }
+  }
+  document.querySelector(".textoDominio").innerHTML=textresult;
 };
